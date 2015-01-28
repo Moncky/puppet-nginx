9a7d973bf1d3d9a1850ebb08a6ad66f7e6d6b6a2
Callum Scott <callum.scott@zonal.co.uk>
2015-01-28 11:01:01 +0000

    fix alingment of =>

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -3,11 +3,11 @@ class nginx::repo::debian {
   include apt
 
   apt::source { "nginx":
-    location => "http://nginx.org/packages/debian/",
-    release => "${lsbdistcodename}",
-    repos => "nginx",
-    key => "7BD9BF62",
-    key_source => "http://nginx.org/keys/nginx_signing.key",
+    location     => "http://nginx.org/packages/debian/",
+    release      => "${lsbdistcodename}",
+    repos        => "nginx",
+    key          => "7BD9BF62",
+    key_source   => "http://nginx.org/keys/nginx_signing.key",
     incklude_src => false,
 
 }
